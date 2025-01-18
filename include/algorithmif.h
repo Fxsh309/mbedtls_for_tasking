97cefe112ca1507ee5e9b34f8cbf073d0f1479c1
Fxsh309 <102196901+Fxsh309@users.noreply.github.com>
2025-01-18 14:41:11 +0800

    Update algorithmif.h

--- include/algorithmif.h
+++ include/algorithmif.h
@@ -5,7 +5,8 @@
  *          including CMAC calculation, RSA signing, verification, encryption, and decryption,
  *          as well as AES encryption and decryption functionality.
  * Author: <kxx> <gss>
- * License: Copyright (C) 2024
+ * version: 1.0
+ * Date: 2024-012-01
  */
 
 #ifndef ALGORITHMIF_H_
@@ -34,6 +35,15 @@ typedef enum {
     ZY_MBEDTLS_MD_SHA3_384 = 0x12,/**< The SHA3-384 message digest. */
     ZY_MBEDTLS_MD_SHA3_512 = 0x13,/**< The SHA3-512 message digest. */
 } mbedtls_md_type_t_1;
+
+
+typedef enum {
+    ZY_MBEDTLS_RSA_PKCS_V15 = 0, /**< Use PKCS#1 v1.5 encoding. */
+    ZY_MBEDTLS_RSA_PKCS_V21 = 1  /**< Use PKCS#1 v2.1 encoding. */
+} mbedtls_rsa_pkcs_version_t_1;
+
+
+extern unsigned char plaint_buffer[256];
 
 /**
  * Function to calculate CMAC using AES-128.
@@ -85,7 +95,8 @@ int Calculate_hash_finish(unsigned char *output);
  */
 int rsa_signature(const unsigned char *priv_key_der, size_t priv_key_len,
                   const unsigned char *signature, size_t *sig_len,
-                  const unsigned char *hash, size_t hash_len);
+                  const unsigned char *hash, size_t hash_len,
+                  mbedtls_rsa_pkcs_version_t_1 padding_mode);
 
 /**
  * Function to verify an RSA signature.
@@ -100,7 +111,8 @@ int rsa_signature(const unsigned char *priv_key_der, size_t priv_key_len,
  */
 int rsa_verify(const unsigned char *der_key, size_t der_key_len,
                const unsigned char *signature, size_t sig_len,
-               const unsigned char *hash, size_t hash_len);
+               const unsigned char *hash, size_t hash_len,
+               mbedtls_rsa_pkcs_version_t_1 padding_mode);
 
 /**
  * Function to encrypt data using RSA with a public key in DER format.
@@ -111,10 +123,12 @@ int rsa_verify(const unsigned char *der_key, size_t der_key_len,
  * @param ilen: Length of the input data in bytes.
  * @param output: Pointer to the output buffer where the encrypted data will be stored.
  * @return 0 if successful, or an error code if unsuccessful.
+ * 
+ * note: The size of encuryted data must be less than 190 bytes.
  */
 int rsa_encrypt(const unsigned char *pub_key_der, size_t pub_key_len,
                 const unsigned char *input, size_t ilen,
-                unsigned char *output);
+                unsigned char *output,mbedtls_rsa_pkcs_version_t_1 padding_mode);
 
 /**
  * Function to decrypt data using RSA with a private key in DER format.
@@ -123,39 +137,73 @@ int rsa_encrypt(const unsigned char *pub_key_der, size_t pub_key_len,
  * @param priv_key_len: Length of the DER encoded private key in bytes.
  * @param input: Pointer to the input data to be decrypted.
  * @param ilen: Length of the input data in bytes.
- * @param output: Pointer to the output buffer where the decrypted data will be stored.
  * @param olen: Pointer to the variable that will receive the length of the decrypted data in bytes.
  * @return 0 if successful, or an error code if unsuccessful.
+ * 
+ * note: The plaint data will be stored in plaint_buffer.
  */
 int rsa_decrypt(const unsigned char *priv_key_der, size_t priv_key_len,
                 const unsigned char *input, size_t ilen,
-                unsigned char *output, size_t *olen);
+                size_t *olen,mbedtls_rsa_pkcs_version_t_1 padding_mode);
 
 /**
  * Function to encrypt data using AES in CBC mode.
  * 
  * @param input: Pointer to the data to be encrypted.
  * @param input_length: Length of the data to be encrypted.
- * @param encrypted_data: Pointer to the buffer where the encrypted data will be stored.
+ * @param encrypted_data: Pointer to the buffer where the encrypted data will be stored,which must be multiple of 16 bytes.
  * @param key: Pointer to the AES encryption key.
+ * @param key_len: Length of the AES encryption key in bytes, which must be 16, 24, or 32.
+ * @param iv: Pointer to the initialization vector (IV).
+ * @param padding: Padding mode, which can be '0' for no padding or '1' for PKCS#7 padding.
  * @return Length of the encrypted data, or a negative value if an error occurred.
  */
-int encrypt_data_by_aes_cbc(unsigned char *input, int input_length, unsigned char *encrypted_data, unsigned char *key);
+int aes_cbc_encrypt(unsigned char *input,int input_length,unsigned char *encrypted_data,
+                unsigned char *key,unsigned char key_len,unsigned char *iv, char padding);
 
 /**
  * Function to decrypt data using AES in CBC mode.
  * 
- * @param encrypted_data: Pointer to the data to be decrypted.
+ * @param encrypted_data: Pointer to the data to be decrypted,which must be multiple of 16 bytes.
  * @param encrypted_length: Length of the encrypted data.
  * @param decrypted_data: Pointer to the buffer where the decrypted data will be stored.
  * @param key: Pointer to the AES decryption key.
+ * @param key_len: Length of the AES decryption key in bytes,which must be 16, 24, or 32.
+ * @param iv: Pointer to the initialization vector (IV),which must be the same as the one used for encryption.
+ * @param padding: Padding mode, which can be '0' for no padding or '1' for PKCS#7 padding.
  * @return Length of the decrypted data, or a negative value if an error occurred.
  */
-int decrypt_data_by_aes_cbc(unsigned char *encrypted_data, int encrypted_length, unsigned char *decrypted_data, unsigned char *key);
+int aes_cbc_decrypt(unsigned char *encrypted_data,int encrypted_length,unsigned char *decrypted_data,
+                    unsigned char *key,unsigned char key_len,unsigned char *iv, char padding);
+
 
 /**
- * Function to run the main test cases for the implemented algorithms.
+ * Encrypts data using AES ECB mode
+ *
+ * @param input: Pointer to the input buffer containing the data to be encrypted.
+ * @param encrypted_data: Pointer to the output buffer where the encrypted data will be stored.
+ * @param key: Pointer to the AES encryption key.
+ * @param key_len: Length of the AES key in bytes, which must be 16, 24, or 32.
+ * @return Length of the encrypted data on success, or a negative value on failure indicating an error.
  */
-void TestMainfunction();
+int aes_ecb_encrypt(unsigned char *input, unsigned char *encrypted_data, unsigned char *key,
+                    unsigned char key_len);
+
+/**
+ * Decrypts data using AES ECB mode
+ *
+ * @param encrypted_data: Pointer to the input buffer containing the data to be decrypted.
+ * @param decrypted_data: Pointer to the output buffer where the decrypted data will be stored.
+ * @param key: Pointer to the AES decryption key.
+ * @param key_len: Length of the AES key in bytes, which must be 16, 24, or 32.
+ * @return Length of the decrypted data on success, or a negative value on failure indicating an error.
+ */
+int aes_ecb_decrypt(unsigned char *encrypted_data, unsigned char *decrypted_data,
+                    unsigned char *key, unsigned char key_len);
+
+int verify_ecdsa_signature(const unsigned char *pub_key, size_t pub_key_len,
+                           const unsigned char *signature, size_t sig_len,
+                           const unsigned char *hash_buf, size_t hash_len);
 
 #endif /* ALGORITHMIF_H_ */
+
